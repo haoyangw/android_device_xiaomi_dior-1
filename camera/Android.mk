f32ad521ac5dce632c6ee484813db22d698cfcc4
Wang Hao Yang <haoyangw@gmail.com>
2015-07-28 17:52:21 +0800

    Add some small touchups

--- camera/Android.mk
+++ camera/Android.mk
@@ -11,7 +11,7 @@ LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE := camera.msm8226
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)

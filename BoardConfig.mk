b2a3e77ad7f627b62ad520d3b081929a230d5f2d
Wang Hao Yang <haoyangw@gmail.com>
2015-07-31 23:17:32 +0800

    Fix prebuilt kernel support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,6 +40,11 @@ TARGET_CPU_VARIANT := krait
 # Kernel
 TARGET_PREBUILT_KERNEL := device/xiaomi/dior/kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/dior
+ifeq ("$(wildcard $(TARGET_KERNEL_SOURCE))","")
+ifneq ("$(wildcard $(TARGET_PREBUILT_KERNEL))","")
+$(shell mkdir -p $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr)
+endif
+endif
 TARGET_KERNEL_CONFIG := cyanogenmod_dior_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=dior user_debug=31 msm_rtb.filter=0x37
 BOARD_KERNEL_BASE := 0x00000000
